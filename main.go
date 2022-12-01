1747bc3549f9928db5aee0364e7321d2e7271cef
Ryano0oceros <getryanfallon@gmail.com>
2022-12-01 08:28:08 -0500

    init post lesson 69

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+	links := []string{
+		"https://google.com",
+		"https://facebook.com",
+		"https://wheretfisryan.com",
+		"https://golang.org",
+	}
+
+	for _, link := range links {
+
+
+	}
+
+}
+
+func httpReq
